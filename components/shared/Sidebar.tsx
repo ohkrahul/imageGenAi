d3db2dba2e0b7f3f3c4718f9e58cef0e071ba8ad
ohkrahul <rahulsahu3612@gmail.com>
2025-01-06 00:52:40 +0530

    done1

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -13,13 +13,13 @@ const Sidebar = () => {
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
-        <Link href="/" className="sidebar-logo">
+        {/* <Link href="/" className="sidebar-logo">
           <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
-        </Link>
+        </Link> */}
 
         <nav className="sidebar-nav">
           <SignedIn>
-            <ul className="sidebar-nav_elements">
+            <ul className="sidebar-nav_elements ">
               {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route === pathname
 
